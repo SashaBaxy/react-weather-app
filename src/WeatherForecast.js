82ff1e1b8477b7fde8b6132eebc5fe91ec312cd3
SashaBaxy <101371820+SashaBaxy@users.noreply.github.com>
2022-10-09 21:27:15 +0100

    changed let loaded to true via setloaded

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -4,21 +4,15 @@ import "./WeatherForecast.css";
 import Axios from "axios";
 
 export default function WeatherForecast(props) {
-    let [loaded, setLoaded] = useState(false);
+    let [loaded, setLoaded] = useState(true);
+    let [forecast, setForecast] = useState(null);
+
     function handleResponse(response) {
-        console.log(response.data);
+        setForecast(response.data.daily);
+        setLoaded(true);
     }
 
     if (loaded) {
-        let apiKey = "b38034b76f133ca05fd67538c5b8c748";
-        let longitude = props.coordinates.lon;
-        let latitude = props.coordinates.lat;
-        let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
-        Axios.get(apiUrl).then(handleResponse);
-
-        return null;
-    } else {
         return (
             <div className="WeatherForecast">
                 <div className="row">
@@ -37,5 +31,14 @@ export default function WeatherForecast(props) {
                 </div>
             </div>
         );
+    } else {
+        let apiKey = "b38034b76f133ca05fd67538c5b8c748";
+        let longitude = props.coordinates.lon;
+        let latitude = props.coordinates.lat;
+        let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+        Axios.get(apiUrl).then(handleResponse);
+
+        return null;
     }
 }
