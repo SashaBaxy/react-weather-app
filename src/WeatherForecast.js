2c4c2972febac242c936e39c40cf43201a57ddef
SashaBaxy <101371820+SashaBaxy@users.noreply.github.com>
2022-10-09 21:19:34 +0100

    Updates WeatherForecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,36 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 import Axios from "axios";
 
 export default function WeatherForecast(props) {
+    let [loaded, setLoaded] = useState(false);
     function handleResponse(response) {
         console.log(response.data);
     }
 
-    let apiKey = "b38034b76f133ca05fd67538c5b8c748";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    if (loaded) {
+        let apiKey = "b38034b76f133ca05fd67538c5b8c748";
+        let longitude = props.coordinates.lon;
+        let latitude = props.coordinates.lat;
+        let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
-    Axios.get(apiUrl).then(handleResponse);
+        Axios.get(apiUrl).then(handleResponse);
 
-    return (
-        <div className="WeatherForecast">
-            <div className="row">
-                <div className="col">
-                    <div className="WeatherForecast-day">Thu</div>
-                    <WeatherIcon code="01d" size={36} />
-                    <div className="WeatherForecast-temperature">
-                        <span className="WeatherForecast-temperature-max">
-                            19°
-                        </span>{" "}
-                        <span className="WeatherForecast-temperature-min">
-                            10°
-                        </span>
+        return null;
+    } else {
+        return (
+            <div className="WeatherForecast">
+                <div className="row">
+                    <div className="col">
+                        <div className="WeatherForecast-day">Thu</div>
+                        <WeatherIcon code="01d" size={36} />
+                        <div className="WeatherForecast-temperature">
+                            <span className="WeatherForecast-temperature-max">
+                                19°
+                            </span>{" "}
+                            <span className="WeatherForecast-temperature-min">
+                                10°
+                            </span>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
