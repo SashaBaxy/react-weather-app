e92219f8ecfa13009eec218c7fccf0b6f3250a8b
SashaBaxy <101371820+SashaBaxy@users.noreply.github.com>
2022-10-09 22:08:01 +0100

    Added to Loop to show 6 day forecast

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -24,10 +24,10 @@ export default function WeatherForecastDay(props) {
         <div>
             <div className="WeatherForecast-day">{day()}</div>
             <WeatherIcon code={props.data.weather[0].icon} size={36} />
-            <div className="WeatherForecast-temperature">
+            <div className="WeatherForecast-temperatures">
                 <span className="WeatherForecast-temperature-max">
                     {maxTemperature()}
-                </span>{" "}
+                </span>
                 <span className="WeatherForecast-temperature-min">
                     {minTemperature()}
                 </span>
